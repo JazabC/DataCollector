efc81eaa261bd35f9b988025ab0b6049ee57add2
Jazab Chaudhry <j3chaudh@edu.uwaterloo.ca>
2019-01-13 17:20:31 -0500

    edited strings

--- app9-datacollector-flask-postgres/send_email.py
+++ app9-datacollector-flask-postgres/send_email.py
@@ -7,7 +7,7 @@ def send_email(email, height, average_height, count):
     to_email=email
 
     subject="Height data"
-    message="Hey there, your height is <strong>%s</strong>. <br> Average height of all is <strong>%s</strong> and that is calculated out of <strong>%s</strong> people. <br> Thanks!" % (height, average_height, count)
+    message="Hello!, Your Height is <strong>%s</strong>. <br> Average height of all is <strong>%s</strong> calculated out of <strong>%s</strong> people. <br> Thanks!" % (height, average_height, count)
 
     msg=MIMEText(message, 'html')
     msg['Subject']=subject
